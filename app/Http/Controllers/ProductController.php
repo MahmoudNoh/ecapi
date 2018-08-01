9b14be3a58575ea78ebc6b2ce8f10334dbffc22e
MahmoudNoh <mahmoudnoh50@gmail.com>
2018-08-01 15:17:06 +0200

    Display all Product and add Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,9 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -50,9 +53,34 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->user_id = $request->user()->id;
+
+         $product->save();
+        return response([
+                    'data' => new ProductResource($product)
+                ],
+                Response::HTTP_CREATED);
+        return response()->json(['status'=>"Successfuly Created Product ", 'data'=>$product], Response::HTTP_CREATED);
+       /* if( $product->save()){
+             return response(
+                [
+                    'status'=> 'success',
+                    'data' => new ProductResource($product)
+                ],
+                Response::HTTP_CREATED);
+        }else{
+
+        }*/
+
+       
     }
 
     /**
